35de5ec0b5671b2ced9db9e49aff18ff5ad3b7e5
Sanjaykondle <101350840+Sanjaykondle@users.noreply.github.com>
2022-04-29 19:08:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+/* scroll on buttons */
+$('.js--scroll-to-plans').click(function(){
+    $('html, body').animate({scrollTop: $('.js-section-plans').offset().top},1000);
+});
+
+
+});
